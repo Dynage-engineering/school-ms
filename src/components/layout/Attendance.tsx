318bd19addeca74a32e01f78e45d205c86457b58
litmus-zhang <abdulsalamlukmon9@gmail.com>
2025-03-26 02:07:16 +0100

    implement modal confirmation for marking attendance; enhance button styling and layout

--- src/components/layout/Attendance.tsx
+++ src/components/layout/Attendance.tsx
@@ -141,7 +141,16 @@ export function Attendance() {
               {userData?.fullname}
             </th>
             <td className="px-6 py-4">
-              <Button onClick={handleAttendance}>Mark Attendance</Button>
+              <button
+                data-modal-target="popup-modal"
+                data-modal-toggle="popup-modal"
+                className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                type="button"
+                onClick={handleAttendance}
+              >
+                Mark Attendance
+              </button>
+              {/* <Button onClick={handleAttendance}></Button> */}
             </td>
             <td className="px-6 py-4">
               <span className="px-2 py-1 text-xs font-semibold leading-tight text-green-700 bg-green-100 dark:bg-green-700 dark:text-green-100 rounded-full dark:rounded-none">
@@ -153,14 +162,87 @@ export function Attendance() {
       </table>
 
       {/* Video element to display camera stream if capturing */}
-      {capturing && (
-        <div className="mt-4 flex flex-col items-center ">
-          <video ref={videoRef} style={{ width: "100%", maxWidth: "400px" }} autoPlay />
-          <Button onClick={captureAndCompare} className="mt-2">
-            Capture & Verify
-          </Button>
+
+      <div
+        id="popup-modal"
+        tabIndex={-1}
+        className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+      >
+        <div className="relative p-4 w-full max-w-md max-h-full">
+          <div className="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
+            <button
+              type="button"
+              className="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+              data-modal-hide="popup-modal"
+            >
+              <svg
+                className="w-3 h-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 14"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                />
+              </svg>
+              <span className="sr-only">Close modal</span>
+            </button>
+            <div className="p-4 md:p-5 text-center">
+              <svg
+                className="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+              <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                Are you sure you want to mark this attendance now?
+              </h3>
+              {/* {capturing && ( */}
+                <div className="mt-4 flex flex-col items-center fixed left-0 top-0 w-full h-full bg-gray-900 bg-opacity-90 z-50">
+                  <video
+                    ref={videoRef}
+                    className="w-64 h-64 rounded-full object-cover"
+                    autoPlay
+                  />
+                  <button
+                data-modal-hide="popup-modal"
+                type="button"
+                onClick={captureAndCompare}
+                className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
+              >
+                Capture & Verify
+              </button>
+              <button
+                data-modal-hide="popup-modal"
+                type="button"
+                className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+              >
+                No, cancel
+              </button>
+                  {/* <Button onClick={captureAndCompare} className="mt-2">
+                    Capture & Verify
+                  </Button> */}
+                </div>
+              {/* )} */}
+              
+            </div>
+          </div>
         </div>
-      )}
+      </div>
 
       {/* Hidden canvas element used for capturing frame */}
       <canvas ref={canvasRef} style={{ display: "none" }} />
